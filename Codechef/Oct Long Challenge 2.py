3b313cc67d926d43c968e7d5aa7fc900edadeb93
Ishant Juyal <ishanjj61@gmail.com>
2020-10-04 23:09:40 +0530

    Edited oct long 2

--- Codechef/Oct Long Challenge 2.py
+++ Codechef/Oct Long Challenge 2.py
@@ -2,6 +2,20 @@ t = int(input())
 for _ in range(t):
     n,k = map(int, input().split())
     q = list(map(int, input().split()))
-    sum_q = sum(q)
-    day = (sum_q//k) + 1
-    print(day)
+    
+    if sum(q) <= 3000000:
+        i = 0
+        queries = 0
+        resolved = 0
+        while True:
+            if i < n:
+                queries += q[i]
+            if queries < k:
+                print(i+1)
+                break
+            else:
+                resolved += k
+                queries -= k
+            i += 1
+    else:
+        print(sum(q)//k + 1)

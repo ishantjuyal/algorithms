abe8a5252d8373d18a739e05fa7226237fe7d097
Ishant Juyal <ishanjj61@gmail.com>
2020-10-09 13:19:22 +0530

    merge arrays

---
+++ General/Merge sorted arrays.py
@@ -0,0 +1,45 @@
+"""
+Merge two sorted arrays without using
+extra space
+"""
+
+print("Enter first sorted array")
+a = list(map(int, input().split()))
+
+print("Enter second sorted array")
+b = list(map(int, input().split()))
+
+i = 0
+j = 0
+
+while True:
+    # print("i", i)
+    # print("j", j)
+    if a[i] >= b[j]:
+        a = a[:i] + [b[j]] + a[i:]
+        i += 1
+        j += 1
+        
+    else:
+        if a[i] < b[j]:
+            # print("i", i)
+            # print("j", j)
+            # print("a[i]", a[i])
+            # print("b[j]", b[j])
+            if i == len(a) -1:
+                a.append(b[j])
+                j += 1
+            elif b[j] <= a[i+1]:
+                a = a[:i+1] + [b[j]] + a[i+1:]
+                i += 1
+                j += 1
+            else:
+                i += 1
+    # print("Sample")
+    # print(a)
+    # print("i", i)
+    # print("j", j)
+    if j >= len(b):
+        break
+
+print(*a)

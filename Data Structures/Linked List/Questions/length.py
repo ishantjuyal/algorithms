8545e144d25eb2ba9a832e1ed2e5f8674eff77e9
Ishant Juyal <ishanjj61@gmail.com>
2021-04-28 19:51:57 +0530

    Find Length Question done

---
+++ Data Structures/Linked List/Questions/length.py
@@ -0,0 +1,48 @@
+"""
+Find Length of a Linked List (Iterative and Recursive)
+"""
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+    
+    # Iterative approach
+    def get_length(self):
+        count = 0
+        temp = self.head
+        while temp:
+            count += 1
+            temp = temp.next
+        return(count)
+    
+    # Recursive approach
+    def get_count_rec(self, node):
+        if (not node):
+            return 0
+        else:
+            return 1 + self.get_count_rec(node.next)
+    def get_count(self):
+        return self.get_count_rec(self.head)
+
+
+# Code execution starts here
+if __name__=='__main__':
+    llist = LinkedList()
+    llist.push(1)
+    llist.push(3)
+    llist.push(1)
+    llist.push(2)
+    llist.push(1)
+    print ("Count of nodes by iterative approach is :",llist.get_length())
+
+    print ("Count of nodes by recursive approach is :",llist.get_count())

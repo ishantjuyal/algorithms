06bb92f4bf563a02534f66c0f35252ff321488c1
Ishant Juyal <ishanjj61@gmail.com>
2021-08-21 12:29:19 +0530

    Removed CP code

--- Codechef/Jan Long Challenge 1.py
+++
@@ -1,9 +0,0 @@
-t = int(input())
-for _ in range(t):
-    n, k, d = map(int, input().split())
-    a = list(map(int, input().split()))
-
-    available_problems = sum(a)
-    days = available_problems//k
-    events = min(d, days)
-    print(events)
